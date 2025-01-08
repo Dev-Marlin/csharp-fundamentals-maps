9c6de3c0ce0b305f5472a3178c344cd969f5a80e
Martin Lind <marlin00.Work@gmail.com>
2025-01-08 11:18:44 +0100

    Cleared the tests for all the methods

--- csharp-fundamentals-maps.Main/Core.cs
+++ csharp-fundamentals-maps.Main/Core.cs
@@ -49,8 +49,8 @@ namespace csharp_fundamentals_maps.Main
         public string getValue(string key)
         {
             
-           
-            return string.Empty;
+       
+            return createPerson()[key];
 
 
         }
@@ -64,9 +64,10 @@ namespace csharp_fundamentals_maps.Main
          */
          public bool hasKey(Dictionary<string,string> dictionary, string isitthere)
          {
-            return false;
-            
-         }
+           
+            return dictionary.ContainsKey(isitthere);
+
+        }
 
 
         //TODO: 3. Modify method named getValueOrDefault that accepts two parameters:
@@ -78,7 +79,11 @@ namespace csharp_fundamentals_maps.Main
          */
         public int getValueOrDefault(Dictionary<string,int> dictionary, string isitthere)
         {
-            return 0;
+            if (!dictionary.ContainsKey(isitthere))
+                return -1;
+
+
+            return dictionary[isitthere];
 
         }
 
@@ -105,6 +110,13 @@ namespace csharp_fundamentals_maps.Main
             map.Add(96, "nice");
             // Write your code below this comment...
 
+            foreach(int number in numbers) 
+            {
+                if(map.ContainsKey(number))
+                {
+                    results.Add(map[number]);
+                }
+            }
            
 
             //    // ...and above this comment
